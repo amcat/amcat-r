d72c81eb51b3eea21dc3db55401cefa6efe8fb63
Wouter van Atteveldt <wouter@vanatteveldt.com>
2014-01-18 14:39:27 +0100

    Move to rstudio project, remove build files

--- R/amcatr.r
+++ R/amcatr.r
@@ -1,6 +1,17 @@
 library(rjson)
 library(RCurl)
 
+#' Connect to the AmCAT API
+#'
+#' Connect to the AmCAT API. Currently simply stores the authentication information
+#' in a list and performs a trivial call to check that the use can log in.
+#' 
+#' @param host the hostname, e.g. http://amcat.vu.nl or http://localhost:8000
+#' @param username the username to login with, e.g. 'amcat'
+#' @param passwd the password to login with, e.g. 'amcat'
+#' @param test if True, make a test call to check the password
+#' @return A list with authentication information that is used by the other functions in this package
+#' @export
 amcat.connect <- function(host, username=NULL, passwd=NULL, test=T) {
   if (is.null(username)) {
     cat(paste("Please enter the username for", host, "\n"))
@@ -18,6 +29,19 @@ amcat.connect <- function(host, username=NULL, passwd=NULL, test=T) {
   conn
 }
 
+#' Get objects from the AmCAT API
+#'
+#' Get a table of objects from the AmCAT API, e.g. projects, sets etc.
+#' 
+#' @param conn the connection object from \code{\link{amcat.connect}}
+#' @param resource the name of the resource, e.g. 'projects'
+#' @param format the format to use, e.g. csv or json. 
+#' @param stepsize the number of objects to retrieve per query. Set to a lower number of larger objects
+#' @param filters a list of filters to pass to the API, e.g. list(project=1)
+#' @param test if True, make a test call to check the password
+#' @param use__in a subset of names(filters) that should be passed with an in argument
+#' @return A dataframe of objects (rows) by properties (columns)
+#' @export
 amcat.getobjects <- function(conn, resource, format='csv', stepsize=50000, filters=list(), use__in=c(), ...) {
   limit = stepsize
   url = paste(conn$host, '/api/v4/', resource, '?format=', format, '&page_size=', limit, sep='')
@@ -44,6 +68,7 @@ amcat.getobjects <- function(conn, resource, format='csv', stepsize=50000, filte
   result
 }
 
+#' Internal call to check GET results and parse as csv or json
 .amcat.readoutput <- function(result, format){
   if (result == '401 Unauthorized')
     stop("401 Unauthorized")
@@ -58,7 +83,13 @@ amcat.getobjects <- function(conn, resource, format='csv', stepsize=50000, filte
   result
 }
 
-
+#' Run an action from the AmCAT API
+#'
+#' @param conn the connection object from \code{\link{amcat.connect}}
+#' @param action the name of the action
+#' @param format the format to request, e.g. csv or json
+#' @return A dataframe containing the result of the action [WvA: shouldn't that depend on the action??]
+#' @export
 amcat.runaction <- function(conn, action, format='csv', ...) {
   resource = 'api/action'
   url = paste(conn$host, resource, action, sep="/")
@@ -78,21 +109,26 @@ amcat.runaction <- function(conn, action, format='csv', ...) {
   result
 }
 
-amcat.articlemeta <- function(conn, articlesets, media=c(), from_date=NA, to_date=NA, filter_columns=c('id','date','medium','length'), time=F){
-  meta = NULL
-  if(length(media) == 0) media = c(NA)
-  
-  for(medium in media) {
-    filters = list(articleset=articlesets)
-    if(!is.na(medium)) filters = c(filters, medium=medium)
-    if(!is.na(from_date)) filters = c(filters, date_from=from_date)
-    if(!is.na(to_date)) filters = c(filters, date_to=to_date)
-    
-    ameta = amcat.getobjects(conn, "articlemeta", filters=filters, use__in=c('articleset'))
-    if(nrow(ameta) == 0) next
-    if(length(filter_columns > 0)) ameta = ameta[,filter_columns]
-    meta = rbind(meta,ameta)
+#' Get article metadata from AmCAT
+#'
+#' Uses the \code{\link{amcat.getobjects}} function to retrieve article metadata, and applies some
+#' additional postprocessing, e.g. to convert the data to Date objects.
+#'
+#' @param conn the connection object from \code{\link{amcat.connect}}
+#' @param sets the article set id(s) to retrieve
+#' @param filters additional filters, e.g. list(medium=1)
+#' @param columns the names of columns to retrieve
+#' @param format the format to request, e.g. csv or json
+#' @param time if true, parse the date as POSIXct datetime instead of Date
+#' @return A dataframe containing the articles and the selected columns
+#' @export
+amcat.articlemeta <- function(conn, sets, filters=list(), columns=c('id','date','medium','length'), time=F){
+  filters[['articleset']] = sets 
+  result = amcat.getobjects(conn, "articlemeta", filters=filters, use__in=c('articleset'))
+  if(length(columns > 0)) result = result[,columns]
+  if ("date" %in% names(result)) {
+    result$date = (if(time == T) as.POSIXct(result$date, format='%Y-%m-%d %H:%M:%S') 
+                   else as.Date(result$date, format='%Y-%m-%d'))
   }
-  if(time == T) meta$date = as.POSIXct(meta$date, format='%Y-%m-%d %H:%M:%S') else meta$date = as.Date(meta$date, format='%Y-%m-%d')
-  meta
+  return(result)
 }
--- R/query.r
+++ R/query.r
@@ -1,3 +1,17 @@
+#' Conduct an aggregate query on amcat
+#'
+#' This function is similar to using the 'show table' function in AmCAT. It allows you to specify a 
+#' number of queries and get the number of hits per search term, per period, etc. 
+#' 
+#' @param conn the connection object from \code{\link{amcat.connect}}
+#' @param queries a vector of queries to run
+#' @param labels if given, labels corresponding to the queries
+#' @param sets one or more article set ids to query on
+#' @param ... additional arguments to pass to the AmCAT API. Use axis1 and axis2 to specify 
+#'            the grouping, possible values are medium, year/month/week/day. Additional filters
+#'            are also possible.
+#' @return A data frame with hits per group
+#' @export
 amcat.aggregate <- function(conn, queries, labels=queries, sets, ...) {
   result = NULL
   for (i in 1:length(queries)) {
