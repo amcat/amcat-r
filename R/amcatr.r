d647745d2c92f01c63ef8c8475e5ca00b454b23d
kasperwelbers <kasperwelbers@gmail.com>
2014-08-21 17:06:52 +0200

    changed trycatch in .amcat.readoutput for compatibility issues

--- R/amcatr.r
+++ R/amcatr.r
@@ -92,16 +92,11 @@ amcat.getURL <- function(conn, path, filters=NULL, post=FALSE, post_options=list
 #' @param path the path of the url to retrieve (using the host from conn)
 #' @param filters: a named vector of filters, e.g. c(project=2, articleset=3)
 #' @param post use HTTP POST instead of GET
-#' @param page: the page number to start retrieving
-#' @param page_size: the number of rows per page
-#' @param max_page: the page number to stop retrieving at, if given
 #' @return dataframe 
-#' @export
-amcat.getpages <- function(conn, path, format='csv', page=1, page_size=1000, filters=NULL, post=FALSE, max_page=NULL) {
+amcat.getpages <- function(conn, path, format='csv', page=1, page_size=1000, filters=NULL, post=FALSE) {
   filters = c(filters, page_size=page_size, format=format)
   result = data.frame()
-  while (TRUE) {
-    if (!is.null(max_page)) if (page > max_page) break
+  while(TRUE){
     page_filters = c(filters, page=page)
     subresult = amcat.getURL(conn, path, page_filters, post=post)
     if (subresult == "") break
@@ -138,7 +133,7 @@ amcat.getobjects <- function(conn, resource, ...) {
   } else  if (format == 'csv') {
     con <- textConnection(result)
     result = tryCatch(read.csv(con), 
-                      error=function(e) {warning(e); data.frame()})
+                      error=function(e) data.frame())
   }
   result
 }
@@ -182,12 +177,11 @@ amcat.runaction <- function(conn, action, format='csv', ...) {
 #' @param time if true, parse the date as POSIXct datetime instead of Date
 #' @param dateparts if true, add date parts (year, month, week)
 #' @param medium_names if true, retrieve medium names and turn medium column into a factor
-#' @param ... additional arguments are passed to \code{\link{amcat.getpages}}. Useful arguments include page_size, page (starting page) and maxpage (end page)
 #' @return A dataframe containing the articles and the selected columns
 #' @export
-amcat.getarticlemeta <- function(conn, set, filters=list(), columns=c('id','date','medium','length'), time=F, dateparts=F, medium_names=T, ...){
+amcat.getarticlemeta <- function(conn, set, filters=list(), columns=c('id','date','medium','length'), time=F, dateparts=F, medium_names=T){
   filters[['articleset']] = set 
-  result = amcat.getobjects(conn, "articlemeta", filters=filters, ...)
+  result = amcat.getobjects(conn, "articlemeta", filters=filters)
   if(length(columns > 0)) result = result[,columns]
   if ("date" %in% names(result)) {
     result$date = (if(time == T) as.POSIXct(result$date, format='%Y-%m-%d %H:%M:%S') 
@@ -234,11 +228,9 @@ amcat.add.articles.to.set <- function(conn, project, articles, articleset=NULL,
     articleset = fromJSON(r)$id
     message("Created articleset ", articleset, ": ", articleset.name," in project ", project)
   }
-  if (!is.null(articles)) {
-    idlist = lapply(articles, function(x) list(id=x))
-    path = paste("", "api", "v4", "projects",project, "articlesets", articleset, "articles", "", sep="/")
-    .amcat.post(conn, path, toJSON(idlist))
-  }
+  idlist = lapply(articles, function(x) list(id=x))
+  path = paste("", "api", "v4", "projects",project, "articlesets", articleset, "articles", "", sep="/")
+  .amcat.post(conn, path, toJSON(idlist))
   articleset
 }
 
@@ -287,54 +279,4 @@ amcat.add.articles.to.set <- function(conn, project, articles, articleset=NULL,
     }
   }
   return(output)
-}
-
-
-#' Upload new articles to AmCAT
-#' 
-#' Upload articles into a given project and article set, or into a new article set if the articleset argument is character
-#' All arguments headline, medium etc. should be either of the same length as text, or of length 1
-#' All factor arguments will be converted to character using as.character
-#' For date, please provide either a string in ISO notatoin (i.e. "2010-12-31" or "2010-12-31T23:59:00")
-#' or a variable that can be converted to string using format(), e.g. Date, POSIXct or POSIXlt. 
-#' The articles will be uploaded in batches of 100. 
-#' 
-#' @param conn the connection object from \code{\link{amcat.connect}}
-#' @param project the project to add the articles to
-#' @param articleset the article set id of an existing set, or the name of a new set to create
-#' @param text the text of the articles to upload
-#' @param headline the headlines of the articles to upload
-#' @param medium the medium of the articles to upload. 
-#' @param provenance if articleset is character, an optional provenance string to store with the new set
-#' @param ... and additional fields to upload, e.g. author, byline etc. 
-#' @export
-amcat.upload.articles <- function(conn, project, articleset, text, headline, date, medium, provenance=NULL, ...) {
-  
-  n = length(text)
-  if (is.character(articleset)) {
-    if (is.null(provenance)) provenance=paste("Uploaded", n, "articles using R function amcat.upload.articles")
-    articleset = amcat.add.articles.to.set(conn, project, articles=NULL, articleset.name=articleset, articleset.provenance=provenance) 
-  }
-  path = paste("", "api", "v4", "projects",project, "articlesets", articleset, "articles", "", sep="/")
-  
-  if (!is.character(date)) date = format(date, "%Y-%m-%dT%H:%M:%S")
-  fields = data.frame(headline=headline, text=text, date=date, medium=medium, ...)
-  # make sure all fields have correct length
-  for (f in names(fields)) {
-    if (is.factor(fields[[f]])) fields[[f]] = as.character(fields[[f]])
-    if (length(fields[[f]]) == 1) fields[[f]] = rep(fields[[f]], n)
-    if (length(fields[[f]]) != n) stop(paste("Field", f, "has incorrect length:", length(fields[[f]]), "should be 1 or ", n))
-  }
-  
-  # not very efficient, but probably not the bottleneck
-  chunks = split(fields, ceiling((1:n)/100))
-  for(chunk in chunks) {
-    json_data = vector("list", nrow(chunk))
-    for (i in seq_along(json_data)) {
-      json_data[[i]] = unlist(lapply(chunk, function(x) x[i]))
-    }
-    json_data = toJSON(json_data)
-    message("Uploading ", nrow(chunk), " articles to set ", articleset)
-    .amcat.post(conn, path, data=json_data)
-  }
 }
