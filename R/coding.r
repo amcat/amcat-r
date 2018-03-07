70f0e132e9fee7d53448d95c33f3b1b5f4b08071
Wouter van Atteveldt <wouter@vanatteveldt.com>
2018-03-07 01:22:09 +0100

    Add articles optino to create codingjob

--- R/coding.r
+++ R/coding.r
@@ -2,12 +2,20 @@
 #' @param conn the connection object from \code{\link{amcat.connect}}
 #' @param project the project to add the articles to
 #' @param articleset the article set id of an existing set, or the name of a new set to create
+#' @param articlesarticle IDs to be added to this coding job. Ignored if articleset is an ID
 #' @param coder the ID of the coder to assign the job to
 #' @param articleschema the ID of the articleschema (may be ommitted)
 #' @param unitschema the ID of the unit (sentence) schema (may be ommitted)
 #' @export
-amcat.create.codingjob <- function(conn, project, codingjobname, articleset=articleset, coder=coder, articleschema=NULL, unitschema=NULL) {
-  data = list(name=codingjobname, project=project, coder=coder, articleschema=articleschema, unitschema=unitschema, articleset=setid)
+amcat.create.codingjob <- function(conn, project, codingjobname, coder, articleschema=NULL, unitschema=NULL, articleset=NULL, articles=NULL) {
+  if (!is.numeric(articleset)) {
+    if (is.null(articles)) stop("Please specify either an article set ID or a vector of article IDs")
+    setname = if (is.null(articleset)) codingjobname else articleset
+    message("Creating new set ", setname, " with ", length(articles), " articles")
+    articleset = amcat.add.articles.to.set(conn, project, articles, articleset.name=setname)
+  }
+  
+  data = list(name=codingjobname, project=project, coder=coder, articleschema=articleschema, unitschema=unitschema, articleset=articleset)
   json_data = toJSON(data)
   url = paste(conn$host, "api", "v4", "projects", project, "codingjobs", "", sep = "/")
   message("Creating codingjob for coder ",coder)
@@ -15,3 +23,4 @@ amcat.create.codingjob <- function(conn, project, codingjobname, articleset=arti
   if (resp$status_code != 201) stop("Error on POST to ",url,": ", resp$status_code, " ", http_status(resp)$reason,"\n", content(resp))
   invisible(content(resp)$id)
 }
+
