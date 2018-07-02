e4b6797cd8827084cf9d03b27c1930e3cb4750d2
Wouter van Atteveldt <wouter@vanatteveldt.com>
2018-07-02 10:35:31 +0200

    Simplify gethiearchy: remove languages and use default code label

--- R/codebook.r
+++ R/codebook.r
@@ -11,37 +11,10 @@
 amcat.gethierarchy <- function(conn, codebook_id, languages=NULL) {
   
   hierarchy = amcat.getobjects(conn, "codebookcode", filters=list(codebook__id=codebook_id))
-  hierarchy = hierarchy[,c("code", "parent")]
-  
-  for (lang in languages) {
-    r = amcat.getobjects(conn, "language", filters=list(label=lang))
-    if (nrow(r) != 1) stop(paste("Searching for language", lang, "gave", nrow(r), "results"))
-    lang_id = r$id
-    labels = amcat.getobjects(conn, "label", filters=list(language__id=lang_id, 
-                                                          code__codebook_codes__codebook__id=codebook_id))
-    labels = labels[, c("code", "label")]
-    colnames(labels) = c("code", paste("label", lang, sep="."))  
-    hierarchy = merge(hierarchy, labels, all.x=T)
-  }
-  
-  if (!is.null(languages)) {
-    labels = rep(NA, nrow(hierarchy))
-    for (lang in languages) {
-      labelcol = paste("label", lang, sep=".")
-      labels[is.na(labels)] = as.character(hierarchy[is.na(labels), labelcol])
-    }
-    labels = factor(labels)
-    hierarchy$parent = labels[match(hierarchy$parent, hierarchy$code)]
-    hierarchy$code = labels
-    
-  }
-  
-  return(hierarchy)
+  lbl = paste(hierarchy$code, hierarchy$label, sep=" - ")
+  parentlbl = lbl[match(hierarchy$parent, hierarchy$code)]
+  data.frame(code=lbl, parent=parentlbl)
 }
-
-
-
-
 
 .codebookcat <- function(hierarchy, depth=0) {
   # depth 0 is root, depth 1 is cat, etc
